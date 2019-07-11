5a0eec37ec694366e2585627a33a4d4ffc3c44b1
Axel Niklasson <axel.niklasson@live.com>
2019-07-11 10:29:21 +0200

    Print out instance id on /

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 import logging
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return f"Hello World from instance {os.getenv('PLCLI_INSTANCE_ID', -1)}"
